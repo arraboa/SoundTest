7962c17751001856463e50f37069795a8bd4db6c
Alexandre ARRAMON <arramo@voodoo.io>
2016-03-04 15:45:06 +0100

    Setting up the project.

---
+++ SoundTest/SoundTest/AppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.h
+//  SoundTest
+//
+//  Created by Alexandre ARRAMON on 04/03/2016.
+//  Copyright (c) 2016 Voodoo. All rights reserved.
+//
+
+
+#import <UIKit/UIKit.h>
+
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
---
+++ SoundTest/SoundTest/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  SoundTest
+//
+//  Created by Alexandre ARRAMON on 04/03/2016.
+//  Copyright (c) 2016 Voodoo. All rights reserved.
+//
+
+
+#import <UIKit/UIKit.h>
+
+
+@interface ViewController : UIViewController
+
+
+@end
