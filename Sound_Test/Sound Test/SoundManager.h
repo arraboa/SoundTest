6132fdc897854fb5144c0d5f71f9fc60c2e393be
Alexandre ARRAMON <arramo@voodoo.io>
2016-03-10 11:29:48 +0100

    gitignore, SoundManager.m.h

---
+++ Sound_Test/Sound Test/SoundManager.h
@@ -0,0 +1,10 @@
+//
+// Created by Alexandre ARRAMON on 10/03/2016.
+// Copyright (c) 2016 Voodoo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface SoundManager : NSObject
+@end
---
+++ Sound_Test/Sound Test/SoundManager.m
@@ -0,0 +1,12 @@
+//
+// Created by Alexandre ARRAMON on 10/03/2016.
+// Copyright (c) 2016 Voodoo. All rights reserved.
+//
+
+#import "SoundManager.h"
+
+
+@implementation SoundManager {
+
+}
+@end
